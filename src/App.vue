e83b8644b456160abf2b44391d71a1173e93bd14
iesus <iesus90@gmail.com>
2019-11-11 14:33:04 -0300

    login/logout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-if="authenticated" to="/Login" v-on:click.native="logout()" replace>Logout</router-link>
+      <router-link v-if="authenticated" to="/" v-on:click.native="logout()" replace>Logout</router-link>
     </div>
     <router-view  @authenticated="setAuthenticated"/>
   </div>
